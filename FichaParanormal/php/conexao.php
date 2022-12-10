cb3f7736562b82925235beab94ebd925a8c1764d
Yuri Gabriel <73195850+yuriCovalski@users.noreply.github.com>
2022-12-09 22:35:26 -0300

    Delete FichaParanormal directory

--- FichaParanormal/php/conexao.php
+++
@@ -1,5 +0,0 @@
-<?php
-// funcao q conecta com o banco de dados, 1 parametro nome do lugar hospedado, 2 usuario,3 senha, 4 nome do banco
-$ligacao = mysqli_connect('localhost','root','','fichaParanormalBanco');
-
-?>
